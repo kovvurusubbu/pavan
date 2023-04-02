d64db7d03747d0bcc0ec63075a027cc73143b2e2
kovvurusubbu <99384027+kovvurusubbu@users.noreply.github.com>
2023-04-02 16:48:56 +0530

    prometheus_grafana_installtion

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
