8d81fb470a1bbe03f0fd157dccc2e1f493c31b24
kovvurusubbu <99384027+kovvurusubbu@users.noreply.github.com>
2023-09-05 15:10:08 +0530

    Update mongo.tf

--- Teeraform/mongo.tf
+++ Teeraform/mongo.tf
@@ -1,7 +1,9 @@
+main.tf 
+
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAXZ6SSYC52EUMIV"
-  secret_key = "69UX4supbh5/KEblM2Ny3yvNGKjTV7Qc"
+  access_key = "AKIAXKDZ6SSYO7D2NDNM"
+  secret_key = "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"
 }
 
 # Generate a new key pair for SSH access to the MongoDB instance
@@ -12,8 +14,7 @@ resource "tls_private_key" "ssh_key" {
 
 resource "local_file" "ssh_key" {
   filename = "${path.module}/mongodb-ssh-key.pem"
-  content  = tls_private_key.ssh_key.private_key_pemyes
-
+  content  = tls_private_key.ssh_key.private_key_pem
 }
 
 # Create the key pair in AWS
@@ -24,15 +25,33 @@ resource "aws_key_pair" "mongodb_ssh_key" {
 
 # Define the MongoDB instance
 resource "aws_instance" "mongodb_instance" {
-  ami           = "ami-0eb02ea7bebc3d987" # Bitnami MongoDB AMI for ap-south-1
+  ami           = "ami-0eb02ea7bebc3d987"
   instance_type = "t2.medium"
-
   subnet_id     = var.subnet_id
   key_name      = aws_key_pair.mongodb_ssh_key.key_name
 
   tags = {
     Name = "MongoDB-Instance"
   }
+
+  /*provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y mongodb",
+      "sudo systemctl start mongodb",
+      "sudo systemctl enable mongodb",
+      "sleep 10",
+      "source ~/.bashrc",
+      "mongo admin --eval 'db.createUser({user:\"subbu\", pwd:\"subbu1234\", roles:[{role:\"root\",db:\"admin\"}]});'",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/mongodb-ssh-key.pem")
+      host        = aws_instance.mongodb_instance.public_ip
+    }
+  }*/
 }
 
 # Security group for MongoDB (assuming this resource exists in your configuration)
@@ -48,3 +67,20 @@ resource "aws_security_group" "mongodb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+---
+var.tf
+
+variable "vpc_id" {
+  description = "The ID of the VPC where the MongoDB instance will be deployed."
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet where the MongoDB instance will be launched."
+}
+---
+
+terraform.tfvars
+
+vpc_id    = "vpc-0704c93e22bf3fe26"
+subnet_id = "subnet-0921cf7a8e5add9f6"
+
